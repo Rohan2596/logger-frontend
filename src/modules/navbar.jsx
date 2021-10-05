d976ac87b6a6276576b36642dbebadaa61846d6c
Rohan2596 <rohankadam662@gmail.com>
2021-10-05 17:03:44 +0530

    ADDED:Menu icons to navbar component.

--- src/modules/navbar.jsx
+++ src/modules/navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import '../css/navbar.css'
 function Navbar() {
- return(
+   function openSideNav(){
+       console.log("Side Nav Opened..");
+   
+      }
+       return(
      <div className="navbar-main-container">
-         
+         <span className="material-icons" onClick={()=>openSideNav()}>segment</span>
          <span className="title" >Logger Platfrom</span>
      </div>
  )
