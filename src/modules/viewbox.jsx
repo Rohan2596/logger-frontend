b6771b4e55a612e4704084ee73530f7179af28a5
Rohan2596 <rohankadam662@gmail.com>
2021-10-05 10:49:31 +0530

    ADDED:Viewbox for message displaying.

---
+++ src/modules/viewbox.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import '../css/viewbox.css'
+function ViewBox() {
+ return(
+     <div className="viewbox-main-container">
+        <div className="logger-message-container">
+           <div className="log-timestamp">05-10-2021 10:30 Tuesday</div>
+           <div className="log-message">Logger message for we  testing</div>
+        </div>
+         
+        <div className="logger-message-container">
+           <div className="log-timestamp">05-10-2021 10:30 Tuesday</div>
+           <div className="log-message">Logger message for we  testing</div>
+        </div>
+
+        <div className="logger-message-container">
+           <div className="log-timestamp">05-10-2021 10:30 Tuesday</div>
+           <div className="log-message">Logger message for we  testing</div>
+        </div>
+        <div className="logger-message-container">
+           <div className="log-timestamp">05-10-2021 10:30 Tuesday</div>
+           <div className="log-message">Logger message for we  testing</div>
+        </div>
+        <div className="logger-message-container">
+           <div className="log-timestamp">05-10-2021 10:30 Tuesday</div>
+           <div className="log-message">Logger message for we  testing</div>
+        </div>
+        <div className="logger-message-container">
+           <div className="log-timestamp">05-10-2021 10:30 Tuesday</div>
+           <div className="log-message">Logger message for we  testing</div>
+        </div>
+     </div>
+ )
+    
+}
+
+export default ViewBox; 
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import '../css/dashboard.css'
 import Navbar from "../modules/navbar";
+import ViewBox from "../modules/viewbox";
 function Dashboard() {
  return(
      <div className="dashboard-main-container">
          <Navbar/>
+         <div className="view-box-container">
+         <ViewBox/>
+         </div>
      </div>
  )
     
