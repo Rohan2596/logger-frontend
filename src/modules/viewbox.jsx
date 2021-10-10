dbf5b24d3a82e394bc9f2ec134ad47652c0cf84a
Rohan2596 <rohankadam662@gmail.com>
2021-10-10 16:18:34 +0530

    INTEGRATION:Api integrated with frontend.

--- src/modules/viewbox.jsx
+++ src/modules/viewbox.jsx
@@ -1,8 +1,57 @@
 import React from "react";
 import '../css/viewbox.css'
+import{useState,useEffect} from'react'
+import axiois from "axios";
 function ViewBox() {
+
+
+   const fetch={}
+   const getRequest={}
+   const app_context={}
+   const[logs,setLogs]=useState([])
+   useEffect(() => {
+   getRequest.limit="100";
+   getRequest.page_no="1";
+   app_context.mode="WEB";
+   app_context.server="tomcat";
+    fetch.request=getRequest;
+    fetch.app_context=app_context;
+
+      fetchLogs(fetch)
+    });
+
+   function fetchLogs(){
+    
+      axiois.post("http://localhost:8080/log/fetch",
+      fetch,{
+         headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            "Access-Control-Allow-Origin": "*",
+           'token' : localStorage.getItem('token') 
+         
+      }})
+      .then((response)=>{
+         if(response.status==200){
+           setLogs(response.data.result)
+           
+         }
+         return response
+      })
+      .catch((error)=>{
+         console.error("Error..",error);
+   });
+   }
+   
+   const messages=logs.map((message)=>{
+      <div className="logger-message-container">
+      <div className="log-timestamp">05-10-2021 10:30 Tuesday</div>
+      <div className="log-message">{message}</div>
+   </div>
+   })  
+
  return(
      <div className="viewbox-main-container">
+        {messages}
         <div className="logger-message-container">
            <div className="log-timestamp">05-10-2021 10:30 Tuesday</div>
            <div className="log-message">Logger message for we  testing</div>
--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,13 +1,55 @@
 import React from 'react';
 import {useHistory } from 'react-router-dom';
 import '../css/user.css'
+import{useState} from'react'
+import axiois from "axios";
 function User() {
   let history = useHistory();
 
+  const user={}
+  const request={};
+  const baseUrl="http://localhost:8080/user/login"
+  const appcontext={};
+
+
+  const[username,setUsername]=useState('');
+  const[password,setPassword]=useState('')
+  
+  function onChangeUserName(event){
+    setUsername(event.target.value)
+
+  }
+  function onChangePassword(event){
+    setPassword(event.target.value)
+
+  }
+  function uselogin(value) {
+    appcontext.mode="WEB";
+    appcontext.server="tomcat"
+    user.request=value;
+    user.app_context=appcontext
+    console.log(user);
+      
+       axiois.post(baseUrl,user)
+       .then((response)=>{
+         if(response.status==200 ){   
+             localStorage.setItem("token",response.data.result);
+             history.push('/dashboard')
+            }
+           return response
+       }).catch((error)=>console.error(error));
+       
+   }
+
+
       function submit(){
-        console.log("Submit");
-        localStorage.setItem("token","success");
-        history.push('/dashboard')
+        
+        request.username=username;
+        request.password=password;
+        console.log(request);
+        uselogin(request)
+        
+       
       }
      return(
        <div className="main-user-container" >
@@ -16,8 +58,8 @@ function User() {
            <div className="auth-input-container">
 
            <img alt="avatar" src="https://img.icons8.com/color/108/000000/circled-user-male-skin-type-3--v2.png"/>
-           <input className="username" type="text" placeholder="What's your username?"></input>
-           <input className="password" type="text" placeholder="What's your password?"></input>
+           <input className="username" type="text" value={username} onChange={(e)=>onChangeUserName(e)} placeholder="What's your username?"></input>
+           <input className="password" type="text" id={password} onChange={(e)=>onChangePassword(e)} placeholder="What's your password?"></input>
            <div className="sign-in" onClick={()=>submit()}>Sign In</div>
            </div>
          </div>
